003cbb207b15a414ce936018585219983ffc13a3
MOHITH508 <malli.mohith2023@vitstudent.ac.in>
2025-07-05 11:15:31 +0530

    Add files via upload

---
+++ lottie_helper.py
@@ -0,0 +1,9 @@
+<script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script>
+<dotlottie-player 
+    src="https://lottie.host/6e15df1d-b5ce-4b24-9f1c-1f396e01671f/KFRyrJ85eT.json" 
+    background="transparent" 
+    speed="1" 
+    style="width: 300px; height: 300px" 
+    loop 
+    autoplay>
+</dotlottie-player>
